a13ac427a06a11005adff459b8bf6806eb31a899
Andrea Gussoni <andreagus@outlook.it>
2022-08-04 12:21:04 +0200

    updated crud operations
    status change op and read whole table op

--- internet_forensics/api/sql_crud.py
+++ internet_forensics/api/sql_crud.py
@@ -1,14 +1,23 @@
 import database_connect
 from internet_forensics.api.database_connect import db_cnx
 
- 
-class crud_ops(database_connect):
 
-    # establishing connection to db, can be greatly costumized in db_cnx class
+class crudOps(database_connect):
 
-    def read_data(connection):
-        connection = db_cnx.db_connect(config="") # configs are passed here
+    # function to read whole table, to be used when user asks for all records of a topic
+    def readWholeTable(table):
+        connection = db_cnx.db_connect(config="")  # configs are passed here
         cursor = connection.cursor
-        
-        cursor.execute() # reading database data, sql commands will be here when db available
 
+        # reading database data from a specific table, %s is not good practice but will not use user input
+        cursor.execute("Select * from (%s)", (table))
+        result = cursor.fetchall()
+        print(result)
+
+    # method to change the status of a record, only for non personal data
+    def delNonPersRecord(table, recordId, status):
+        connection = db_cnx.db_connect(config="")  # configs are passed here
+        cursor = connection.cursor
+
+        cursor.execute("UPDATE %s SET status= %s WHERE recordId= %s",
+                       (table, status, recordId))
