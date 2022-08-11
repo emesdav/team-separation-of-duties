860db44ea0104b1004466823251509263b7ff7b2
Marianne Manaog <manaog.mariannelyne@gmail.com>
2022-08-11 19:07:46 +0100

    Changed the format of the log file to .log following Andrea's suggestion.

--- internet_forensics/src/internet_forensics/logging/constants.py
+++ internet_forensics/src/internet_forensics/logging/constants.py
@@ -4,4 +4,4 @@ The purpose of this file is to maintain constants used for logging.
 
 DATE_TIME_FMT = '%Y-%m-%d %H:%M:%S'
 
-LOG_FILE_NAME_W_EXT = 'custom_logs.txt'
+LOG_FILE_NAME_W_EXT = 'custom_logs.log'
