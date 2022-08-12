0dda4f508b2294f4c0cea64800277ab47e8be0de
Andrea Gussoni <andreagus@outlook.it>
2022-08-12 11:06:41 +0200

    Requested changes

--- internet_forensics/api/database_connect.py
+++ internet_forensics/api/database_connect.py
@@ -1,10 +1,23 @@
-from errno import errorcode
 import pyodbc
+import logging
 
 """ 
 This is a default blank script to connect to our database that will be filled 
 with db informations and security measures when will be developed, it will be called after
 user authentication with the necessary credentials
+"""
+
+"""
+To connect to DB we'll need the configuration data drom the DB creation which will be provided when the DB is ready
+here is an example:
+connection_string = (
+    'DRIVER=MySQL ODBC 8.0 ANSI Driver;'
+    'SERVER=localhost;'
+    'DATABASE=mydb;'
+    'UID=root;'
+    'PWD=mypassword;'
+    'charset=utf8mb4;'
+)
 """
 
 
@@ -17,6 +30,6 @@ class db_cnx():
             self.cursor = cnx.cursor()
         except pyodbc.Error as ex:
             sqlstate = ex.args[1]
-            print(sqlstate)
+            logging.debug(sqlstate)
         else:
             cnx.close()
--- internet_forensics/api/routes.py
+++ internet_forensics/api/routes.py
@@ -1,10 +1,26 @@
-from flask import Flask, session
+from flask import Flask, session, redirect, url_for
+
+"""
+The purpose of this file is to contain all the necessary routes to connect the application interface to the DB
+"""
 
 app = Flask(__name__)
+
+"""
+Landing route that tells if the user is already logged in
+will use username string to check if present in session memory
+"""
 
 
 @app.route('/')
 def index():
     if 'username' in session:
         return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    # will complete as soon as we have a structure of DB
+    username = str(input())
