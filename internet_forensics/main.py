172a3c6d690b1088850e06c3ff3d2d9b86f09c75
mw509 <moonwalker509@gmail.com>
2022-08-14 14:34:20 +0200

    separation of concerns
    improved on the user manager and included SOC

--- internet_forensics/main.py
+++ internet_forensics/main.py
@@ -3,25 +3,94 @@ from datetime import date, timedelta
 from user_manager.user_manager import UserManager
 from main_constants import *
 from internet_forensics.validation.validate import Validate
+from internet_forensics.user_manager import user_manager_constants as user_constants
+
+fresh_start = True
 
 
 @click.command()
 def main():
     # log start of application
-    click.echo(WELCOME_SCREEN_MESSAGE)
+    if fresh_start:
+        click.echo(WELCOME_SCREEN_MESSAGE)
+
     response = click.prompt(MAIN_MENU_OPTION_LIST)
 
-    while not Validate(response).if_integer():
+    restart = False
+
+    while not Validate(response).if_integer() or restart:
+        restart = False
         if response == "1":
             main_user_login()
         else:
             click.echo(WRONG_MENU_OPTION_INPUT + " : " + response)
-            click.echo(APPLICATION_EXIT_MESSAGE) #TODO: Application should not exit but provide an option to try again
+            restart = True
 
 
+@click.command()
 def main_user_login():
-    user = UserManager
-    user.user_login()
+    global fresh_start
+    # Action to be decided on
+    login_attempts_count = 0
+    total_allowed_login_attempts = 3
+
+    # Local method to count login. I just did not wa to repeat code here. I hate repeating code
+    def login_count_response():
+        return True if login_attempts_count < total_allowed_login_attempts else False
+
+    username = password = ""
+    while not Validate(username).if_email() and login_count_response():
+        username = click.prompt(user_constants.PROMPT_USERNAME)
+        login_attempts_count += 1
+        click.echo(user_constants.LOGIN_ATTEMPT_COUNT_REMAINING % {"attempts_count": login_attempts_count,
+                                                                   "allowed_attempts": total_allowed_login_attempts})
+
+    while not Validate(password).if_string() and login_count_response():
+        password = click.prompt(user_constants.PROMPT_PASSWORD)
+        login_attempts_count += 1
+        click.echo(user_constants.LOGIN_ATTEMPT_COUNT_REMAINING % {"attempts_count": login_attempts_count,
+                                                                   "allowed_attempts": total_allowed_login_attempts})
+
+    if login_count_response():
+        user = UserManager
+        user.user_login()
+    else:
+        # Let's move on
+        fresh_start = False
+        main()
+        # It will be a great add to display something that says we started all over again
+
+
+@click.command()
+def user_registration():
+    # Instructions
+    firstname = lastname = address = email = password = ""
+
+    while not Validate(firstname).if_string():
+        firstname = click.prompt(user_constants.PROMPT_PASSWORD)
+
+    while not Validate(lastname).if_string():
+        lastname = click.prompt(user_constants.PROMPT_PASSWORD)
+
+    while not Validate(address).if_string():
+        address = click.prompt(user_constants.PROMPT_PASSWORD)
+
+    while not Validate(email).if_string():
+        email = click.prompt(user_constants.PROMPT_PASSWORD)
+
+    while not Validate(password).if_string():
+        password = click.prompt(user_constants.PROMPT_PASSWORD)
+
+    click.echo(user_constants.DATA_VERIFIED)
+    # We can now proceed
+
+    click.echo(user_constants.LOGIN_VALIDATION_FAILED)
+
+    # Per response, process request
+    # We simply calculate 30days from the day of request to delete the data
+    deactivation_date = str(date.today() + timedelta(days=30))
+
+    click.echo(deactivation_date)
 
 
 if __name__ == "__main__":
--- internet_forensics/user_manager/user_manager.py
+++ internet_forensics/user_manager/user_manager.py
@@ -2,85 +2,47 @@ import click
 from datetime import date, timedelta
 from .user_manager_constants import *
 from internet_forensics.validation.validate import Validate
+from internet_forensics.validation.sanitize import Sanitize
 
 
 # TODO: NOTE: Adding the self argument triggers an error.this should be fixed
 # TODO: For the purpose of loosely coupling the whole system, we may want to introduce dependency injections
 class UserManager:
-    def __init__():
-        print(WELCOME_MESSAGE)
+    def __init__(self, username, password="", firstname="", lastname="", address="", email=""):
+        self.username = Sanitize(username).input_data()
+        self.password = Sanitize(password).input_data()
+        self.firstname = Sanitize(lastname).input_data()
+        self.lastname = Sanitize(lastname).input_data()
+        self.address = Sanitize(address).input_data()
+        self.email = Sanitize(email).input_data()
+        self.user_id = ""
 
-    @click.command()
-    def user_login():
-        # We will count the number of times login is attempted and take an action
+    def user_login(self):
+        # Harsh Password
 
-        # Action to be decided on
-        login_attempts_count = 0
-        total_allowed_login_attempts = 3
+        # Query DB for username and password
 
-        # Local method to count login. I just did not wa to repeat code here. I hate repeating code
-        def login_count_response():
-            return True if login_attempts_count < total_allowed_login_attempts else False
+        return self.user_id
 
-        # Let's set Login instructions and validations here
-        click.echo(VERIFYING_DATA)
-
-        username = password = ""
-        while not Validate(username).if_email() and login_count_response():
-            username = click.prompt(PROMPT_USERNAME)
-            login_attempts_count += 1
-            click.echo(LOGIN_ATTEMPT_COUNT_REMAINING % {"attempts_count": login_attempts_count,
-                                                        "allowed_attempts": total_allowed_login_attempts})
-
-        while not Validate(password).if_string() and login_count_response():
-            password = click.prompt(PROMPT_PASSWORD)
-            login_attempts_count += 1
-            click.echo(LOGIN_ATTEMPT_COUNT_REMAINING % {"attempts_count": login_attempts_count,
-                                                        "allowed_attempts": total_allowed_login_attempts})
-
-            # We can now proceed
-            # Check DB
-            # If record is found and matches return true else false
-        if login_count_response():
-            click.echo(LOGIN_VALIDATION_FAILED)
-        else:
-            # Let's move on
-            pass
-
-        # Per response, process request
-        # We simply calculate 30days from the day of request to delete the data
-        deactivation_date = str(date.today() + timedelta(days=30))
-
-        click.echo(deactivation_date)
-
-    @click.command()
-    def user_registration():
-
+    def user_password_reset(self):
         # Instructions
-        firstname = lastname = address = email = password = ""
 
-        while not Validate(firstname).if_string():
-            firstname = click.prompt(PROMPT_PASSWORD)
+        # Query DB for username and if found, select email and send password reset code to email
 
-        while not Validate(lastname).if_string():
-            lastname = click.prompt(PROMPT_PASSWORD)
+        # Insert new password request into password request table with a 2hour time expiry
 
-        while not Validate(address).if_string():
-            address = click.prompt(PROMPT_PASSWORD)
+        # If all processes completed, return true else return false
 
-        while not Validate(email).if_string():
-            email = click.prompt(PROMPT_PASSWORD)
+        return self.user_id # ignore this line. Intentional
 
-        while not Validate(password).if_string():
-            password = click.prompt(PROMPT_PASSWORD)
+    def user_creation(self):
+        # Instructions
 
-        click.echo(DATA_VERIFIED)
-        # We can now proceed
-        
-        click.echo(LOGIN_VALIDATION_FAILED)
+        # Insert Data into DB
 
-        # Per response, process request
-        # We simply calculate 30days from the day of request to delete the data
-        deactivation_date = str(date.today() + timedelta(days=30))
+        # return userID
 
-        click.echo(deactivation_date)
+        return self.user_id
+
+
+
