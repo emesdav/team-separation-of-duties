8036c20cee62a61199468ac6abca1557e0f3500e
mw509 <moonwalker509@gmail.com>
2022-08-22 22:57:40 +0200

    Modified the Validation
    Modified the validation to return a simple boolean for if value is valid or not and as agreed in previous meeting.

--- src/internet_forensics/validate/validation.py
+++ src/internet_forensics/validate/validation.py
@@ -22,7 +22,7 @@ custom_logger = generate_custom_logger(name=NAME_OF_DATA_VAL_LOG)
 class Validate:
     """
     This class validates if a value were either an integer, a float, a string, or a valid e-mail address, and
-    returns it via the relevant method if so.
+    returns a boolean
     """
 
     def __init__(self, value: Union[float, int, str]) -> None:
@@ -33,9 +33,10 @@ class Validate:
         """
         if value is not None:
             self.value = value
-            custom_logger.debug(f"{'The value passed is: '}{self.value}")
+            custom_logger.info(f"{'The value passed is: '}{self.value}")
         else:
-            raise ValueError("No value has been provided. Please add a value correctly.")
+            custom_logger.info("No value has been provided. Please add a value correctly.")
+            return False
 
     def __str__(self) -> str:
         return f"<{__class__.__name__} having the input value '{self.value}' to validate>"
@@ -44,13 +45,13 @@ class Validate:
         return f"{__class__.__name__}: (value: '{self.value}')"
 
     @threaded
-    def validate_integer(self) -> int:
+    def if_integer(self) -> int:
         """
-        This function validates if a value were an integer and returns it if so.
+        This function validates if a value were an integer and returns a boolean
 
         Returns:
-            self.value: int
-                      the integer value if validated; otherwise, it logs an error.
+            boolean
+                      for if value was a valid integer or not
         """
 
         if isinstance(self.value, int):
@@ -58,18 +59,19 @@ class Validate:
               f"{'The following valid integer value has been passed: '}{self.value}"
             )
             if self.value._state == STATE_FOR_THREAD:
-                return self.value._result
+                return True
         else:
-            raise ValueError(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'integer: '}{self.value}")
+            custom_logger.info(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'integer: '}{self.value}")
+            return False
 
     @threaded
-    def validate_string(self) -> str:
+    def if_string(self) -> str:
         """
-        This function validates if a value were a string and returns it if so.
+        This function validates if a value were a string and returns a boolean
 
         Returns:
-            self.value: str
-                      the string value if validated; otherwise, it logs an error.
+            boolean
+                      for if value was a valid string or not
         """
 
         if isinstance(self.value, str):
@@ -77,18 +79,19 @@ class Validate:
               f"{'The following valid string value has been passed: '}{self.value}"
             )
             if self.value._state == STATE_FOR_THREAD:
-                return self.value._result
+                return True
         else:
-            raise ValueError(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'string: '}{self.value}")
+            custom_logger.info(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'string: '}{self.value}")
+            return False
 
     @threaded
-    def validate_float(self) -> float:
+    def if_float(self) -> float:
         """
-        This function validates if a value were a float and returns it if so.
+        This function validates if a value were a float and returns a boolean
 
         Returns:
-            self.value: float
-                      the float value if validated; otherwise, it logs an error.
+            boolean
+                      for if value was a valid float or not
         """
 
         if isinstance(self.value, float):
@@ -96,30 +99,32 @@ class Validate:
               f"{'The following valid float value has been passed: '}{self.value}"
             )
             if self.value._state == STATE_FOR_THREAD:
-                return self.value._result
+                return True
         else:
-            raise ValueError(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'float: '}{self.value}")
+            custom_logger.info(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'float: '}{self.value}")
+            return False
 
     @threaded
-    def validate_email(self) -> str:
+    def if_email(self) -> str:
         """
         This function validates if a value were a valid e-mail address based on a RegEx pattern defined in the constant
         EMAIL_VALID_PATTERN and returns it if so.
 
         Returns:
-            self.value: str
-                      the string value with a valid e-mail address if validated; otherwise, it logs an error.
+            boolean
+                      for if value was a valid email address or not
         """
 
         # Enforce to return False as 'fullmatch' returns None.
         is_email_valid = False if fullmatch(EMAIL_VALID_PATTERN, self.value) is None else self.value
 
         if not is_email_valid:
-            raise ValueError(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'e-mail address: '}{self.value}")
+            custom_logger.info(f"{FIRST_SUB_STRING_VAL_LOG_MSG}{'e-mail address: '}{self.value}")
+            return False
         else:
             custom_logger.info(
               f"{'The following valid e-mail address has been passed: '}{self.value}"
             )
 
         if is_email_valid._state == STATE_FOR_THREAD:
-            return is_email_valid._result
+            return True
