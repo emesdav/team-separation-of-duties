b7ebfd0d368a2628c39d83538c42009565f8127a
Andrea Gussoni <andreagus@outlook.it>
2022-08-22 12:37:44 +0200

    new fixes and new crime records table

--- src/internet_forensics/database/db.py
+++ src/internet_forensics/database/db.py
@@ -3,16 +3,21 @@ from sqlalchemy import Column, String, Integer, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 # this connects to db
-engine = create_engine('sqlite:///local.db')
+engine = create_engine('sqlite:///../datastore/local.db')
 
 # this is to manage tables
 base = declarative_base()
 
 
-class users (base):
+class Users(base):
 
     __tablename__ = 'users'
 
+    '''
+    these are the rows to be created inside the users table the privacy part represents:
+    False if not agreed, True if agreed
+    GDPR privacy follows the same reasoning but is divided in two levels, marketing and necessary
+    '''
     user_id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
@@ -37,6 +42,23 @@ class users (base):
         self.privacy = privacy
         self.GDPR_necessary = GDPR_necessary
         self.GDPR_marketing = GDPR_marketing
+
+
+class CrimeRecords(base):
+
+    __tablename__ = 'CrimeRecords'
+
+    name_of_suspect = Column(String, primary_key=True)
+    num_of_repeated_crimes = Column(Integer)
+    type_of_crime = Column(String)
+    address_of_suspect = Column(String)
+
+    def __init__(self, num_of_repeated_crimes, type_of_crime, name_of_suspect, address_of_suspect):
+
+        self.num_of_repeated_crimes = num_of_repeated_crimes
+        self.type_of_crime = type_of_crime
+        self.name_of_suspect = name_of_suspect
+        self.address_of_suspect = address_of_suspect
 
 
 # creating DB and table that resembles the class just created, only if not created yet
--- src/internet_forensics/database/query.py
+++ src/internet_forensics/database/query.py
@@ -1,7 +1,10 @@
 
-from db import users, engine
+from db import Users as users, engine
 from sqlalchemy.orm import sessionmaker
 from src.internet_forensics.encryption.encrypt import Encrypt
+from ..logging.custom_logger import generate_custom_logger
+
+custom_logger = generate_custom_logger("DB queries")
 
 # creating session to talk to db
 Session = sessionmaker(bind=engine)
@@ -15,12 +18,12 @@ class Queries:
         this method checks if the users exists, compares password with encrypted one and returns the id
         """
         for instance in session.query(users).filter(users.email == username).first():
-            if Encrypt.check_pssw(password, instance.password):
+            if Encrypt.check_password(password, instance.password):
                 return instance.user_id
 
     def personal_data(self, user_id: int) -> object:
         """
-        this method looks for a record corresponding to the user id and returns all his informations
+        this method looks for a record corresponding to the user id and returns all their informations
         """
         for instance in session.query(users).filter(users.user_id == user_id).first():
             return instance
@@ -28,7 +31,7 @@ class Queries:
     def signup(self, user_id: int, first_name: str, last_name: str, address: str, email: str, mobile: int,
                password: str, privacy: bool, GDPR_necessary: bool, GDPR_marketing: bool) -> int:
         """
-        This method lets a user signup inserting all his information and returns the id to the program
+        This method lets a user signup inserting all their information and returns the id to the program
         """
 
 
@@ -40,24 +43,25 @@ class Queries:
         try:
             session.add(user)
             session.commit()
-        except Exception as hello:
-            print(hello)
+        except Exception as ex:
+            custom_logger.info(ex)
 
 
         return user.user_id
 
-    def privacy(self, user_id: int, bool: bool) -> bool:
+    def privacy(self, user_id: int, bool_p: bool) -> bool:
         """
-        This method sets the preference for user privacy True = yes, False = no
+        This method sets the preference for user privacy True = yes, False = no to privacy conditions
         """
         for instance in session.query(users).filter(users.user_id == user_id).first():
-            instance.privacy = bool
+            instance.privacy = bool_p
             session.commit()
             return instance.privacy
 
     def GDPR(self, user_id: int, bool_nec: bool, bool_mark: bool) -> bool:
         """
-        This method sets the preference for user GDPRs True = yes, False = no
+        This method sets the preference for user GDPRs True = yes, False = no to GDPR privacy conditions,
+        nec stands for necessary and mark for marketing
         """
         for instance in session.query(users).filter(users.user_id == user_id).first():
             instance.GDPR_necessary = bool_nec
