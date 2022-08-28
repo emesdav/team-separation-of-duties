857880a3064ad9a421d17c85f7deea7226188acf
mw509 <moonwalker509@gmail.com>
2022-08-28 15:03:51 +0200

    fix/logger
    Logger fixed to create all logs in one location

--- src/internet_forensics/logging/constants.py
+++ src/internet_forensics/logging/constants.py
@@ -1,12 +1,15 @@
 """
 The purpose of this file is to maintain constants used for logging.
 """
+import os
 
 DATE_TIME_FMT = '%Y-%m-%d %H:%M:%S'
 
 EMPTY_STRING = ''
 
-FOLDER_NAME_LOG_FILE = 'app_log'
+LOG_FILE_NAME_W_EXT = 'custom_logs.log'
+
+FOLDER_NAME_LOG_FILE = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'app_log')
 
 # The format of each log message includes the timestamp, the
 # logger's name or purpose, the filename,
@@ -17,4 +20,4 @@ FORMAT_OF_LOG_MSG = \
     '[%(asctime)s %(name)s] (%(filename)s %(lineno)d): ' \
     '%(levelname)s %(message)s'
 
-LOG_FILE_NAME_W_EXT = 'custom_logs.log'
+
